e90f76c33dcf918386f7ad7ac46f4d434ed5db65
Nathanuhhu <mahessutan90@gmail.com>
2024-08-14 01:15:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Force HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+});
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
