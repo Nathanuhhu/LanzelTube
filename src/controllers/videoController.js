532345f38d37fad4632a8a0b19dd7ff0300214c6
Nathanuhhu <mahessutan90@gmail.com>
2024-08-17 01:30:23 +0200

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,39 @@
+const Video = require('../models/Video');
+const multer = require('multer');
 const path = require('path');
 
-// Simulate a video database
-const videos = [
-  { id: 1, title: 'Sample Video', description: 'A sample video', filePath: '/uploads/sample.mp4' }
-];
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
 
-exports.getVideoPage = (req, res) => {
-  const videoId = parseInt(req.params.id);
-  const video = videos.find(v => v.id === videoId);
+const upload = multer({ storage });
+
+exports.uploadVideo = [upload.single('video'), async (req, res) => {
+  const { title, description } = req.body;
+  const video = new Video({
+    title,
+    description,
+    filePath: '/uploads/' + req.file.filename,
+    uploadedBy: req.session.userId
+  });
+  await video.save();
+  res.redirect('/');
+}];
+
+exports.getVideos = async (req, res) => {
+  const videos = await Video.find().populate('uploadedBy', 'username');
+  res.json(videos);
+};
+
+exports.getVideoById = async (req, res) => {
+  const video = await Video.findById(req.params.id).populate('uploadedBy', 'username');
   if (video) {
-    res.sendFile(path.join(__dirname, '../../public/video.html'));
+    res.json(video);
   } else {
     res.status(404).send('Video not found');
   }
