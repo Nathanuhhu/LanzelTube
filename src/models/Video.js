1ce45a366ba3586604032f7180011bba6573a6eb
Nathanuhhu <mahessutan90@gmail.com>
2024-08-17 01:32:50 +0200

    Create Video.js

---
+++ src/models/Video.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  filePath: { type: String, required: true },
+  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  uploadedAt: { type: Date, default: Date.now },
+  views: { type: Number, default: 0 },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
+module.exports = mongoose.model('Video', videoSchema);
