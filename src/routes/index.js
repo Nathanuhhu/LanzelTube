5cf1a641529deeed14e0d296fc5dfb7aee6821e6
Nathanuhhu <mahessutan90@gmail.com>
2024-08-17 00:48:28 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,19 @@
 const express = require('express');
+const router = express.Router();
 const path = require('path');
-const bodyParser = require('body-parser');
+const videoController = require('../controllers/videoController');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Force HTTPS
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
+// Home page
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Import routes
-const authRoutes = require('./auth');
-const videoRoutes = require('./video');
-const commentRoutes = require('./comment');
-
-app.use('/auth', authRoutes);
-app.use('/video', videoRoutes);
-app.use('/comment', commentRoutes);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Upload page
+router.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/upload.html'));
 });
+
+// Video page
+router.get('/video/:id', videoController.getVideoPage);
+
+module.exports = router;
